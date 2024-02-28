febd0886078e8a47a664a4e2a322aaa32f182028
and <and@and>
2024-02-28 13:34:17 -0300

    implementado mycontacts

--- webdev/settings.py
+++ webdev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i2&ut=ay5#^gq=7-kh)n05bnt7ap0jk8rxm_a33dm5sa@0m80p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["andersonfontes.pythonanywhere.com"]
+ALLOWED_HOSTS = ["andersonfontes.pythonanywhere.com", "localhost"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mycontacts',
 ]
 
 MIDDLEWARE = [
