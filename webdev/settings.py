04ef6135104e9ced922665d265e3a553bc68d4dd
and <and@and>
2024-03-01 01:50:11 -0300

    uncomment STATIC_URL

--- webdev/settings.py
+++ webdev/settings.py
@@ -119,7 +119,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATICFILES_DIRS = [
 #     BASE_DIR / "mycontacts/static"
 # ]
-# STATIC_URL = "static/"
+STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
