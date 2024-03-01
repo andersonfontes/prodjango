ea03c472571b4347ac67a23a20bf904eaea6f01c
and <and@and>
2024-03-01 01:48:40 -0300

    redundacy clean...

--- webdev/settings.py
+++ webdev/settings.py
@@ -116,10 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    BASE_DIR / "mycontacts/static"
-]
-STATIC_URL = "static/"
+# STATICFILES_DIRS = [
+#     BASE_DIR / "mycontacts/static"
+# ]
+# STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
