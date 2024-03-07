9f9211f1f1163703b8f6b0fb4b62e1b11f30033f
and <and@and>
2024-03-07 14:01:16 -0300

    refazendo pra tirar os prints

--- webdev/settings.py
+++ webdev/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "static") #necessário para o deploy
+STATIC_ROOT = os.path.join(BASE_DIR, "static") #necessário para o deploy
 
 
 # STATICFILES_DIRS = [
