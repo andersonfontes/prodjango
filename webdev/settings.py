9fdcf0eefb60fa043d68202407c3324515b4a14d
and <and@and>
2024-03-07 14:21:30 -0300

    fechando o bendito colchete

--- webdev/settings.py
+++ webdev/settings.py
@@ -120,7 +120,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static") #necessário para o deploy
 
 STATICFILES_DIRS = [
     BASE_DIR / "mycontacts/static"
-
+]
 
 # ]# Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
